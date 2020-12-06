aeb9298a288e3dbd2e9a61e98bd7ce8cb50a2e1e
Karl Oczadly <karl@oczadly.uk>
2020-12-06 17:52:13 +0000

    Complete day 3.

---
+++ src/main/java/uk/oczadly/karl/aoc20/day3/Day3Part1.java
@@ -0,0 +1,63 @@
+package uk.oczadly.karl.aoc20.day3;
+
+import uk.oczadly.karl.aoc20.Helper;
+
+import java.util.List;
+
+/**
+ * @author Karl Oczadly
+ */
+public class Day3Part1 {
+    
+    public static void main(String[] args) throws Exception {
+        Map map = Map.load(Helper.readInputs("3"));
+        
+        int collisions = 0, x = 0, y = 0;
+        do {
+            if (map.isTree(x, y))
+                collisions++;
+            x += 3;
+            y += 1;
+        } while (y < map.height);
+        System.out.printf("Tree collisions: %d%n", collisions);
+    }
+    
+    
+    static class Map {
+        boolean[][] pixels;
+        int width, height;
+    
+        Map(boolean[][] pixels, int width, int height) {
+            this.pixels = pixels;
+            this.width = width;
+            this.height = height;
+        }
+        
+        public boolean isTree(int x, int y) {
+            if (y >= height) throw new IllegalArgumentException("Y position out of range.");
+            return pixels[y][x % width];
+        }
+        
+        public static Map load(List<String> input) {
+            int height = input.size(), width = input.get(0).length();
+            boolean[][] pixels = new boolean[height][width];
+            
+            for (int row = 0; row < input.size(); row++) {
+                char[] chars = input.get(row).toCharArray();
+                for (int col = 0; col < chars.length; col++) {
+                    pixels[row][col] = parseMapPixel(chars[col]);
+                }
+            }
+            return new Map(pixels, width, height);
+        }
+        
+        private static boolean parseMapPixel(char c) {
+            switch (c) {
+                case '.': return false;
+                case '#': return true;
+                default: throw new IllegalArgumentException("Unknown map pixel type.");
+            }
+        }
+    }
+    
+}
---
+++ src/main/java/uk/oczadly/karl/aoc20/day3/Day3Part2.java
@@ -0,0 +1,85 @@
+package uk.oczadly.karl.aoc20.day3;
+
+import uk.oczadly.karl.aoc20.Helper;
+
+import java.util.List;
+
+/**
+ * @author Karl Oczadly
+ */
+public class Day3Part2 {
+    
+    public static void main(String[] args) throws Exception {
+        List<TraversalStrategy> strategies = List.of(
+                new TraversalStrategy(1, 1),
+                new TraversalStrategy(3, 1),
+                new TraversalStrategy(5, 1),
+                new TraversalStrategy(7, 1),
+                new TraversalStrategy(1, 2));
+        
+        Map map = Map.load(Helper.readInputs("3"));
+        
+        long solution = 1;
+        for (TraversalStrategy strategy : strategies) {
+            int collisions = 0, x = 0, y = 0;
+            do {
+                if (map.isTree(x, y))
+                    collisions++;
+                x += strategy.deltaX;
+                y += strategy.deltaY;
+            } while (y < map.height);
+            solution *= collisions;
+            System.out.printf("Tree collisions: %d%n", collisions);
+        }
+        
+        System.out.printf("Solution: %d%n", solution);
+    }
+    
+    
+    static class Map {
+        boolean[][] pixels;
+        int width, height;
+    
+        Map(boolean[][] pixels, int width, int height) {
+            this.pixels = pixels;
+            this.width = width;
+            this.height = height;
+        }
+        
+        public boolean isTree(int x, int y) {
+            if (y >= height) throw new IllegalArgumentException("Y position out of range.");
+            return pixels[y][x % width];
+        }
+        
+        public static Map load(List<String> input) {
+            int height = input.size(), width = input.get(0).length();
+            boolean[][] pixels = new boolean[height][width];
+            
+            for (int row = 0; row < input.size(); row++) {
+                char[] chars = input.get(row).toCharArray();
+                for (int col = 0; col < chars.length; col++) {
+                    pixels[row][col] = parseMapPixel(chars[col]);
+                }
+            }
+            return new Map(pixels, width, height);
+        }
+        
+        private static boolean parseMapPixel(char c) {
+            switch (c) {
+                case '.': return false;
+                case '#': return true;
+                default: throw new IllegalArgumentException("Unknown map pixel type.");
+            }
+        }
+    }
+    
+    static class TraversalStrategy {
+        int deltaX, deltaY;
+    
+        public TraversalStrategy(int deltaX, int deltaY) {
+            this.deltaX = deltaX;
+            this.deltaY = deltaY;
+        }
+    }
+    
+}
