b4f06b384ec7df45ce9f7df4bccbb4983e93c259
Karl Oczadly <karl@oczadly.uk>
2020-12-10 19:35:47 +0000

    Clean up day 5 code.

--- src/main/java/uk/oczadly/karl/aoc20/day5/Day5Part1.java
+++ src/main/java/uk/oczadly/karl/aoc20/day5/Day5Part1.java
@@ -2,26 +2,26 @@ package uk.oczadly.karl.aoc20.day5;
 
 import uk.oczadly.karl.aoc20.Helper;
 
-import java.util.OptionalInt;
-
 /**
  * @author Karl Oczadly
  */
 public class Day5Part1 {
     
-    public static void main(String[] args) throws Exception {
-        OptionalInt maxSeatId = Helper.streamInput(5)
+    public static void main(String[] args) {
+        int maxSeatId = Helper.streamInput(5)
                 .map(Day5Part1::parseSeat)
                 .mapToInt(Seat::getId)
-                .max();
+                .max().getAsInt();
 
-        maxSeatId.ifPresent(id -> System.out.printf("Max seat ID: %d%n", id));
+        System.out.printf("Max seat ID = %d%n", maxSeatId);
     }
     
     
-    /** Parses a Seat object (row and column) from a string input */
+   /** Parses a Seat object (row and column) from a string input */
     private static Seat parseSeat(String str) {
-        return new Seat(calcBinaryNumber(str.substring(0, 7)), calcBinaryNumber(str.substring(7, 10)));
+        return new Seat(
+                calcBinaryNumber(str.substring(0, 7)),
+                calcBinaryNumber(str.substring(7, 10)));
     }
     
     /** Calculates the binary number from a sequence of steps. */
@@ -30,18 +30,14 @@ public class Day5Part1 {
         for (char c : str.toCharArray()) {
             int range = upper - lower + 1;
             switch (c) {
-                case 'F':
-                case 'L':
-                    upper -= range / 2;
-                    break;
-                case 'B':
-                case 'R':
-                    lower += range / 2;
-                    break;
-                default: throw new IllegalArgumentException("Unknown movement.");
+                case 'F': case 'L':
+                    upper -= range / 2; break;
+                case 'B': case 'R':
+                    lower += range / 2; break;
+                default: throw new IllegalArgumentException("Unknown movement char.");
             }
         }
-        if (lower != upper) throw new AssertionError("Unknown result value.");
+        if (lower != upper) throw new AssertionError();
         return lower;
     }
     
--- src/main/java/uk/oczadly/karl/aoc20/day5/Day5Part2.java
+++ src/main/java/uk/oczadly/karl/aoc20/day5/Day5Part2.java
@@ -7,7 +7,7 @@ import uk.oczadly.karl.aoc20.Helper;
  */
 public class Day5Part2 {
     
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         // Populate seats array (true = occupied)
         boolean[][] seats = new boolean[128][8];
         Helper.streamInput(5)
@@ -19,7 +19,7 @@ public class Day5Part2 {
             for (int col = 0; col < 8; col++) {
                 // Ensure seat is unoccupied, and has an occupied seat in the rows before and after
                 if (!seats[row][col] && seats[row - 1][col] && seats[row + 1][col]) {
-                    System.out.printf("Missing seat: (%d, %d) = %d%n", row, col, getId(row, col));
+                    System.out.printf("Missing seat = (%d, %d) = %d%n", row, col, getId(row, col));
                     break loop;
                 }
             }
@@ -29,7 +29,9 @@ public class Day5Part2 {
     
     /** Parses a Seat object (row and column) from a string input */
     private static Seat parseSeat(String str) {
-        return new Seat(calcBinaryNumber(str.substring(0, 7)), calcBinaryNumber(str.substring(7, 10)));
+        return new Seat(
+                calcBinaryNumber(str.substring(0, 7)),
+                calcBinaryNumber(str.substring(7, 10)));
     }
     
     /** Calculates the binary number from a sequence of steps. */
@@ -38,18 +40,14 @@ public class Day5Part2 {
         for (char c : str.toCharArray()) {
             int range = upper - lower + 1;
             switch (c) {
-                case 'F':
-                case 'L':
-                    upper -= range / 2;
-                    break;
-                case 'B':
-                case 'R':
-                    lower += range / 2;
-                    break;
-                default: throw new IllegalArgumentException("Unknown movement.");
+                case 'F': case 'L':
+                    upper -= range / 2; break;
+                case 'B': case 'R':
+                    lower += range / 2; break;
+                default: throw new IllegalArgumentException("Unknown movement char.");
             }
         }
-        if (lower != upper) throw new AssertionError("Unknown result value.");
+        if (lower != upper) throw new AssertionError();
         return lower;
     }
     
