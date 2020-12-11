1cdc44d7240a8b1011c436d78238c6b2890ae793
Karl Oczadly <karl@oczadly.uk>
2020-12-11 15:50:20 +0000

    Complete day 11.

---
+++ src/main/java/uk/oczadly/karl/aoc20/day11/Day11Part2.java
@@ -0,0 +1,126 @@
+package uk.oczadly.karl.aoc20.day11;
+
+import uk.oczadly.karl.aoc20.Helper;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Karl Oczadly
+ */
+public class Day11Part2 {
+    
+    public static void main(String[] args) {
+        SeatMap seatMap = SeatMap.load(Helper.loadInput(11));
+        while (seatMap.nextIteration());
+        
+        int occupied = (int)Arrays.stream(seatMap.seatData).flatMap(Arrays::stream)
+                .filter(s -> s == SeatState.SEAT_OCCUPIED)
+                .count();
+    
+        System.out.printf("Number of occupied seats = %d%n", occupied);
+    }
+    
+    
+    static class SeatMap {
+        private SeatState[][] seatData, nextSeatData;
+        private final int width, height;
+    
+        public SeatMap(SeatState[][] seatData, int width, int height) {
+            this.seatData = seatData;
+            this.nextSeatData = new SeatState[height][width];
+            this.width = width;
+            this.height = height;
+        }
+        
+        
+        public SeatState getSeat(int row, int col) {
+            if (row < 0 || row >= height || col < 0 || col >= width)
+                return SeatState.FLOOR;
+            return seatData[row][col];
+        }
+    
+        public boolean nextIteration() {
+            // Solve each seat
+            boolean hasChanged = false;
+            for (int row = 0; row < height; row++) {
+                for (int col = 0; col < width; col++) {
+                    SeatState currentState = getSeat(row, col);
+                    SeatState nextState = nextIteration(row, col);
+                    nextSeatData[row][col] = nextState;
+                    if (currentState != nextState)
+                        hasChanged = true;
+                }
+            }
+            
+            // Apply changes
+            SeatState[][] oldArr = seatData; // Reuse existing array
+            seatData = nextSeatData;
+            nextSeatData = oldArr;
+            return hasChanged;
+        }
+    
+        private SeatState nextIteration(int row, int col) {
+            SeatState state = getSeat(row, col);
+            if (state != SeatState.FLOOR) {
+                // Count neighbours
+                int neighbours = 0;
+                for (int r = -1; r <= 1; r++) {
+                    for (int c = -1; c <= 1; c++) {
+                        if (hasNeighbour(row, col, r, c)) neighbours++;
+                    }
+                }
+                // Mutate
+                if (state == SeatState.SEAT_EMPTY && neighbours == 0) {
+                    return SeatState.SEAT_OCCUPIED;
+                } else if (state == SeatState.SEAT_OCCUPIED && neighbours >= 5) {
+                    return SeatState.SEAT_EMPTY;
+                }
+            }
+            return state;
+        }
+        
+        private boolean hasNeighbour(int row, int col, int deltaRow, int deltaCol) {
+            if (deltaRow == 0 && deltaCol == 0) return false;
+            int r = row + deltaRow, c = col + deltaCol;
+            while (r >= 0 && r < height && c >= 0 && c < width) {
+                SeatState state = getSeat(r, c);
+                if (state != SeatState.FLOOR)
+                    return state == SeatState.SEAT_OCCUPIED;
+                r += deltaRow;
+                c += deltaCol;
+            }
+            return false;
+        }
+        
+        public static SeatMap load(List<String> input) {
+            int width = input.get(0).length();
+            SeatState[][] arr = new SeatState[input.size()][width];
+            for (int row = 0; row < input.size(); row++) {
+                String rowData = input.get(row);
+                for (int col = 0; col < rowData.length(); col++) {
+                    arr[row][col] = SeatState.valueOfChar(rowData.charAt(col));
+                }
+            }
+            return new SeatMap(arr, width, input.size());
+        }
+    }
+    
+    enum SeatState {
+        FLOOR         ('.'),
+        SEAT_EMPTY    ('L'),
+        SEAT_OCCUPIED ('#');
+        
+        final char character;
+        SeatState(char character) {
+            this.character = character;
+        }
+        
+        public static SeatState valueOfChar(char c) {
+            for (SeatState seat : SeatState.values())
+                if (seat.character == c) return seat;
+            throw new IllegalArgumentException("Unknown seat char.");
+        }
+    }
+    
+}
